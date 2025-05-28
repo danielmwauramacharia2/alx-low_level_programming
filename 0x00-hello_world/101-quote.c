f7210a1e72ce25227531cb9042792df0843be283
danielmwauramacharia2 <danielmwaura303@gmail.com>
2025-05-28 00:14:10 -0700

    Accessing Posix Os and printing a message in the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+/**
+ * main - Entry point of our program
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	int i = 0;
+	char n = '\n';
+	const char *msg= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	while (msg[i] != '\0')
+	{
+		write(2, &msg[i], 1);
+		i++;
+	}
+	write(2, &n, 1);
+	return (1);
+}
