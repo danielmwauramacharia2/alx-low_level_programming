b5703bba348bbb3081aa20874272ea448963118f
danielmwauramacharia2 <danielmwaura303@gmail.com>
2025-05-27 22:46:20 -0700

    working with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of our program
+ *
+ * Return: 0 on success and 1 on failure
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
