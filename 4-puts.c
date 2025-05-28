9275dc29ea504947c1c4662c82411e132884e101
danielmwauramacharia2 <danielmwaura303@gmail.com>
2025-05-27 22:22:33 -0700

    a code that prints a text then followed by a new line

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success 1 on fail
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
