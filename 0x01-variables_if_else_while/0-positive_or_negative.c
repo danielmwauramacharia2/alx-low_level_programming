b47893a8fad8c85d6787222e03492640c2042e3e
danielmwauramacharia2 <danielmwaura303@gmail.com>
2025-06-02 06:42:17 -0700

    a code that check if the randomly generated number is +ve. -ve or 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the code
+ *
+ * Description: Checks if number is +ve, -ve or zero
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
