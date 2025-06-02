ba8263c9370a5eb168fff9bf35df4ca53fda9309
danielmwauramacharia2 <danielmwaura303@gmail.com>
2025-06-01 23:56:39 -0700

    a code that prints lower case alphabets with their corresponding integer

---
+++ alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on sucess
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		printf("%d" ":" "%c", c, c);
+                if (c == 'z');
+			printf("\n");
+		c++;
+	}
+	return (0);
+}
