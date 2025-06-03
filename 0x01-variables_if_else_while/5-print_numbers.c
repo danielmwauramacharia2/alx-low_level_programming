5689648c0e48cfd0f2684d02bd86449aac99f0f7
danielmwauramacharia2 <danielmwaura303@gmail.com>
2025-06-03 02:33:56 -0700

    a code that prints digits to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints single digit number to base 10
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <  10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
