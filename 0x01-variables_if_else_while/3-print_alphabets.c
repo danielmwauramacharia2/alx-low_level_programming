71a236f830d0424948878c403777476ccc1ee9bc
danielmwauramacharia2 <danielmwaura303@gmail.com>
2025-06-03 02:06:29 -0700

    a code that prints lowercase and uppercase consecutively

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point to the program
+ *
+ * Description: Prints lowercase and uppercase alphabets
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
