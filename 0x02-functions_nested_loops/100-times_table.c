bbb442779f3a75bd3d45a70e28918a2edb2d373a
danielmwauramacharia2 <danielmwaura303@gmail.com>
2025-06-23 07:46:51 -0700

    update code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,9 +6,12 @@
  */
 void print_times_table(int n)
 {
-	int i = 0, j, mul;
+	int i, j, mul;
 
-	while (i <= n && (!(n > 15 || n < 0)))
+	if (n > 15 || n < 0)
+		return;
+
+	while (i <= n)
 	{
 		j = 0;
 		while (j <= n)
@@ -16,29 +19,28 @@ void print_times_table(int n)
 			mul = i * j;
 			if (j == 0)
 				_putchar(mul + '0');
-			else if (mul < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(mul + '0');
-			}
-			else if (mul < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((mul / 10) + '0');
-				_putchar((mul % 10) + '0');
-			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((mul / 100) + '0');
-				_putchar(((mul / 10) % 10) + '0');
-				_putchar((mul % 10) + '0');
+				if (mul < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(mul + '0');
+				}
+				else if (mul < 100)
+				{
+					_putchar(' ');
+					_putchar((mul / 10) + '0');
+					_putchar((mul % 10) + '0');
+				}
+				else
+				{
+					_putchar((mul / 100) + '0');
+					_putchar(((mul / 10) % 10) + '0');
+					_putchar((mul % 10) + '0');
+				}
 			}
 			j++;
 		}
