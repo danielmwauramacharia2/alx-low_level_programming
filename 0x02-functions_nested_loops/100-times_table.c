0e24ec347fbc2f475de36584d56d69d0b64471dd
danielmwauramacharia2 <danielmwaura303@gmail.com>
2025-06-23 07:49:42 -0700

    betty number of lines fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,6 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 		return;
-
 	while (i <= n)
 	{
 		j = 0;
@@ -48,16 +47,3 @@ void print_times_table(int n)
 		i++;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
